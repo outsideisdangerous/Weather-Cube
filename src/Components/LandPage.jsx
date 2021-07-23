b2ac72d27df399b4809b87cc53bdcea6100f305b
outsideisdangerous <80612633+outsideisdangerous@users.noreply.github.com>
2021-07-23 22:10:58 +1000

    cleanup

--- src/Components/LandPage.jsx
+++ src/Components/LandPage.jsx
@@ -10,8 +10,6 @@ function LandPage({ location, setLocation }) {
 
   const handleChange = (event) => {
     setLocation(event.target.value);
-    // if (event.target.value.length > 2) {
-    // disabled = false;
     setGeoCodes([]);
     setSevenDays([]);
   };
@@ -23,50 +21,22 @@ function LandPage({ location, setLocation }) {
       setGeoCodes(geoResults);
       setLoading(false);
     } catch (error) {
-      console.log(error.message);
+      alert(error.message);
     }
   };
-
-  // useEffect(() => {
-  //   // 1. Use then-catch chaining
-  //   fetchGeoCode(location).then((geoCodes) => {
-  //     console.log({ geoCodes });
-  //   });
-  // }, [location]);
-
-  // useEffect(() => {
-  //   // 2. Define the async function within use effect
-  //   const fetchGeoCodeAsync = async () => {
-  //     const geoCodes = await fetchGeoCode(location);
-  //     console.log({ geoCodes });
-  //   };
-  //   fetchGeoCodeAsync();
-  // }, [location]);
-
-  // let geoCodes = []; // This is a variable, but NOT a state
-
-  // 3. Using useCallback, can move this inside a custom hook.
-  const fetchGeoCodeAsync = useCallback(async () => {
-    try {
-      const _geoCodes = await fetchGeoCode(location); // returns an array
-      setGeoCodes(_geoCodes);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }, [location]);
 
   const handleGeoLocation = async (geoCode) => {
-    setLoading(true);
-    const { lat, lon } = geoCode;
-    const sevenDay = await fetchWeather(lat, lon);
-    setSevenDays(sevenDay);
-    setLoading(false);
+    try {
+      setLoading(true);
+      const { lat, lon } = geoCode;
+      const sevenDay = await fetchWeather(lat, lon);
+      setSevenDays(sevenDay);
+      setLoading(false);
+      setGeoCodes([geoCode]);
+    } catch (error) {
+      alert(error.message);
+    }
   };
-
-  // const iconURL = (sevenDay) => {
-  //   const icon = sevenDay.weather[0].main
-  //   const URL = "http://openweathermap.org/img/wn/'icon'@2x.png";
-  // }
 
   return (
     <>
@@ -106,8 +76,8 @@ function LandPage({ location, setLocation }) {
             const { icon, main } = sevenDay.weather[0];
             return (
               <li>
-                <span>{moment(sevenDay.dt * 1000).format("DD/MM/YY")}</span>
-                {main}
+                {`${moment(sevenDay.dt * 1000).format("DD/MM/YY")} ${main}`}
+
                 <img
                   src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
                   alt=""
